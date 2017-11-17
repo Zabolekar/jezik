9ed05eba9e20d68099f1ccf662b3b3762d152b95
Svetozar Lashin <niekas33@gmail.com>
2017-11-17 20:04:27 +0200

    Redisplayes accents, edited variables

--- sryaml.py
+++ sryaml.py
@@ -16,12 +16,12 @@ GramInfo = NamedTuple("Accents", [
 ])
 
 def deaccentize(text: str) -> str:
-   accents = '̥́̀̄̆̏̑'
-   accented = {'ȁȃáàā': 'a', 'ȅȇéèē': 'e', 'ȉȋíìī': 'i',
-               'ȕȗúùū': 'u', 'ȑȓŕ': 'r', 'ȀȂÁÀĀ': 'A',
-               'ȄȆÉÈĒ': 'E', 'ȈȊÍÌĪ': 'I', 'ȔȖÚÙŪ': 'U',
-               'ȐȒŔ': 'R', 'ӣѝ': 'и', 'ѐ': 'е',
-               'ӢЍ': 'И', 'Ѐ': 'Е'}
+   accents = '\u0301\u0300\u0304\u0306\u030f\u0311\u0302\u0325'
+   accented = {'ȁȃâáàā': 'a', 'ȅȇêéèē': 'e', 'ȉȋîíìī': 'i',
+               'ȕȗûúùū': 'u', 'ȑȓŕ': 'r', 'ȀȂÂÁÀĀ': 'A',
+               'ȄȆÊÉÈĒ': 'E', 'ȈȊÎÍÌĪ': 'I', 'ȔȖÛÚÙŪ': 'U',
+               'ȐȒŔ': 'R', 'ȍȏôóòō': 'o', 'ȌȎÔÓÒŌ': 'O',
+               'ӣѝ': 'и', 'ѐ': 'е', 'ӢЍ': 'И', 'Ѐ': 'Е'}
    for accent in accents:
       text = text.replace(accent, '')
    for letters in accented:
@@ -39,7 +39,7 @@ def decipher(sequence: str) -> GramInfo:
       line_accents = sequence[:begin_R] if '@' in sequence else sequence[:begin_AP]
       Rs = sequence[begin_R:begin_AP] if '@' in sequence else None
       accents = Accents(
-          {int(i): '̥' for i in Rs[1:].split(',')} if Rs else {},
+          {int(i): '\u0325' for i in Rs[1:].split(',')} if Rs else {},
           {int(i[:-1]): i[-1] for i in line_accents.split(',')} if line_accents else {}
       )
       AP = sequence[begin_AP:begin_MP]
@@ -49,34 +49,34 @@ def decipher(sequence: str) -> GramInfo:
 
    return GramInfo(accents, AP, MP)
 
-def insert(word: str, dict_: Dict[int, str]) -> str:
+def insert(word: str, position_to_accent: Dict[int, str]) -> str:
 
-   list_ = sorted(dict_.keys())
-   first = [0] + list_
-   second = list_ + [None]
+   sorted_keys = sorted(position_to_accent_.keys())
+   first = [0] + sorted_keys
+   second = sorted_keys + [None]
    pieces = [word[first[0]:second[0]]]
 
    for y in range(1, len(first)):
-      pieces.append(dict_[list_[y-1]])
+      pieces.append(position_to_accent[sorted_keys[y-1]])
       pieces.append(word[first[y]:second[y]])
 
    return ''.join(pieces)
 
 def accentize(word: str, sequence: str) -> str:
-   real_accent = {'`': '̀', '´': '́', '¨': '̏', '^': '̑', '_': '̄'}
-   acc_dict = decipher(sequence).accents
-   if acc_dict.v:
-      if acc_dict.r: # now we put the magic ring
-         word = insert(word, acc_dict.r)
+   real_accent = {'`': '\u0300', '´': '\u0301', '¨': '\u030f', '^': '\u0311', '_': '\u0304'}
+   accents = decipher(sequence).accents
+   if accents.v:
+      if accents.r: # now we put the magic ring
+         word = insert(word, accents.r)
       # after that we create a dict with letter numbers representing vowels
       syllabic = 0
-      vow_dict: Dict[int, str] = {}
+      position_to_accent: Dict[int, str] = {}
       for i, letter in enumerate(word):
-         if letter in 'aeiouAEIOUаеиоуАЕИОУ̥':
+         if letter in 'aeiouAEIOUаеиоуАЕИОУ\u0325':
             syllabic += 1
-            if syllabic in acc_dict.v:
-               vow_dict[i+1] = real_accent[acc_dict.v[syllabic]]
-      return insert(word, vow_dict) # then we insert accents into word!
+            if syllabic in accents.v:
+               position_to_accent[i+1] = real_accent[accents.v[syllabic]]
+      return insert(word, position_to_accent) # then we insert accents into word!
    else:
       return word
 
