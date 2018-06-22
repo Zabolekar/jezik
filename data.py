6059e687a91c9b6400ed2dbef617fcb86489cef7
Zabolekar <syekirin@gmail.com>
2018-06-22 12:27:43 +0200

    restricting how our yaml file can be read, just in case

--- data.py
+++ data.py
@@ -10,7 +10,7 @@ dir_path = path.dirname(path.realpath(__file__))
 file_path = path.join(dir_path, "a_sr_ru.yaml")
 
 with open(file_path, encoding="utf-8") as f:
-   raw_data = yaml.load(f)
+   raw_data = yaml.safe_load(f)
    for full_key in raw_data.keys():
       # full_key is with disambiguator, key is without
       key = full_key.split()[0]
