098f34a781edc66ed46d2ecd3e9ca02e0a549b06
Svetozar Lashin <niekas33@gmail.com>
2018-07-04 19:01:44 +0300

    trying to fix verbs

--- paradigms.py
+++ paradigms.py
@@ -41,6 +41,7 @@ class GramInfo:
       self.AP: List[str] = []
       for inf in infos:
          if inf:
+            print(inf)
             line_accents, AP, MP = inf.split('|')
             if '@' in inf:
                Rs, Vs = line_accents.split('@')
@@ -206,43 +207,43 @@ class VerbEnding(NamedTuple):
    theme: AccentedTuple
    ending: AccentedTuple
 
-LabeledEnding = Tuple[str, VerbEnding]
+LabeledEnding = Tuple[NamedTuple, NamedTuple]
 
-class Present(NamedTuple):
-   prs1sg: VerbEnding
-   prs2sg: VerbEnding
-   prs3sg: VerbEnding
-   prs1pl: VerbEnding
-   prs2pl: VerbEnding
-   prs3pl: VerbEnding
-   imv2sg: VerbEnding
-   imv1pl: VerbEnding
-   imv2pl: VerbEnding
+class Present(List[NamedTuple]):
+   prs1sg: List[VerbEnding]
+   prs2sg: List[VerbEnding]
+   prs3sg: List[VerbEnding]
+   prs1pl: List[VerbEnding]
+   prs2pl: List[VerbEnding]
+   prs3pl: List[VerbEnding]
+   imv2sg: List[VerbEnding]
+   imv1pl: List[VerbEnding]
+   imv2pl: List[VerbEnding]
 
    @property
    def labeled_endings(self) -> Iterator[LabeledEnding]:
       yield from zip(self._fields, super().__iter__())
 
-class Past(NamedTuple):
-   pfMsg: VerbEnding
-   pfFsg: VerbEnding
-   pfNsg: VerbEnding
-   pfMpl: VerbEnding
-   pfFpl: VerbEnding
-   pfnNpl: VerbEnding
-   aor1sg: VerbEnding
-   aor2sg: VerbEnding
-   aor3sg: VerbEnding
-   aor1pl: VerbEnding
-   aor2pl: VerbEnding
-   aor3pl: VerbEnding
-   infinitive: VerbEnding
-   ipf1sg: VerbEnding
-   ipf2sg: VerbEnding
-   ipf3sg: VerbEnding
-   ipf1pl: VerbEnding
-   ipf2pl: VerbEnding
-   ipf3pl: VerbEnding
+class Past(List[NamedTuple]):
+   pfMsg: List[VerbEnding]
+   pfFsg: List[VerbEnding]
+   pfNsg: List[VerbEnding]
+   pfMpl: List[VerbEnding]
+   pfFpl: List[VerbEnding]
+   pfnNpl: List[VerbEnding]
+   aor1sg: List[VerbEnding]
+   aor2sg: List[VerbEnding]
+   aor3sg: List[VerbEnding]
+   aor1pl: List[VerbEnding]
+   aor2pl: List[VerbEnding]
+   aor3pl: List[VerbEnding]
+   infinitive: List[VerbEnding]
+   ipf1sg: List[VerbEnding]
+   ipf2sg: List[VerbEnding]
+   ipf3sg: List[VerbEnding]
+   ipf1pl: List[VerbEnding]
+   ipf2pl: List[VerbEnding]
+   ipf3pl: List[VerbEnding]
 
    @property
    def labeled_endings(self) -> Iterator[LabeledEnding]:
@@ -296,113 +297,113 @@ ending_xu = AccentedTuple('ху', '')
 ending_ti = AccentedTuple('ти', '')
 
 i_past = Past(
-   VerbEnding(i_theme_past, AccentedTuple('ʌ', '')),
-   VerbEnding(i_theme_past, AccentedTuple('ла', '')),
-   VerbEnding(i_theme_past, AccentedTuple('ло', '')),
-   VerbEnding(i_theme_past, AccentedTuple('ли', '')),
-   VerbEnding(i_theme_past, AccentedTuple('ле', '')),
-   VerbEnding(i_theme_past, AccentedTuple('ла', '')),
-   VerbEnding(i_theme_past, ending_x),
-   VerbEnding(AccentedTuple('и·', 's0'), AccentedTuple('0·~', 'o.p.p:r.r:')),
-   VerbEnding(AccentedTuple('и·', 's0'), AccentedTuple('0·~', 'o.p.p:r.r:')),
-   VerbEnding(i_theme_past, ending_smo),
-   VerbEnding(i_theme_past, ending_ste),
-   VerbEnding(i_theme_past, ending_she),
-   VerbEnding(i_theme_past, ending_ti),
-   VerbEnding(i_theme_ipf, ending_x),
-   VerbEnding(i_theme_ipf, ending_she),
-   VerbEnding(i_theme_ipf, ending_she),
-   VerbEnding(i_theme_ipf, ending_smo),
-   VerbEnding(i_theme_ipf, ending_ste),
-   VerbEnding(i_theme_ipf, ending_xu)
+   [VerbEnding(i_theme_past, AccentedTuple('ʌ', ''))],
+   [VerbEnding(i_theme_past, AccentedTuple('ла', ''))],
+   [VerbEnding(i_theme_past, AccentedTuple('ло', ''))],
+   [VerbEnding(i_theme_past, AccentedTuple('ли', ''))],
+   [VerbEnding(i_theme_past, AccentedTuple('ле', ''))],
+   [VerbEnding(i_theme_past, AccentedTuple('ла', ''))],
+   [VerbEnding(i_theme_past, ending_x)],
+   [VerbEnding(AccentedTuple('и·', 's0'), AccentedTuple('0·~', 'o.p.p:r.r:'))],
+   [VerbEnding(AccentedTuple('и·', 's0'), AccentedTuple('0·~', 'o.p.p:r.r:'))],
+   [VerbEnding(i_theme_past, ending_smo)],
+   [VerbEnding(i_theme_past, ending_ste)],
+   [VerbEnding(i_theme_past, ending_she)],
+   [VerbEnding(i_theme_past, ending_ti)],
+   [VerbEnding(i_theme_ipf, ending_x)],
+   [VerbEnding(i_theme_ipf, ending_she)],
+   [VerbEnding(i_theme_ipf, ending_she)],
+   [VerbEnding(i_theme_ipf, ending_smo)],
+   [VerbEnding(i_theme_ipf, ending_ste)],
+   [VerbEnding(i_theme_ipf, ending_xu)]
 )
 
 a_past = Past(
-   VerbEnding(a_theme_past, AccentedTuple('ʌ0·', 'm.')),
-   VerbEnding(a_theme_past, AccentedTuple('ла0·', 'm.')),
-   VerbEnding(a_theme_past, AccentedTuple('ло0·', 'm.')),
-   VerbEnding(a_theme_past, AccentedTuple('ли0·', 'm.')),
-   VerbEnding(a_theme_past, AccentedTuple('ле0·', 'm.')),
-   VerbEnding(a_theme_past, AccentedTuple('ла0·', 'm.')),
-   VerbEnding(a_theme_past, ending_x),
-   VerbEnding(AccentedTuple('а·', 'l.p:x.y0'), AccentedTuple('0·~', 'o.k:m.n.t.')),
-   VerbEnding(AccentedTuple('а·', 'l.p:x.y0'), AccentedTuple('0·~', 'o.k:m.n.t.')),
-   VerbEnding(a_theme_past, ending_smo),
-   VerbEnding(a_theme_past, ending_ste),
-   VerbEnding(a_theme_past, ending_she),
-   VerbEnding(a_theme_past, ending_ti),
-   VerbEnding(a_theme_ipf, ending_x),
-   VerbEnding(a_theme_ipf, ending_she),
-   VerbEnding(a_theme_ipf, ending_she),
-   VerbEnding(a_theme_ipf, ending_smo),
-   VerbEnding(a_theme_ipf, ending_smo),
-   VerbEnding(a_theme_ipf, ending_xu)
+   [VerbEnding(a_theme_past, AccentedTuple('ʌ0·', 'm.'))],
+   [VerbEnding(a_theme_past, AccentedTuple('ла0·', 'm.'))],
+   [VerbEnding(a_theme_past, AccentedTuple('ло0·', 'm.'))],
+   [VerbEnding(a_theme_past, AccentedTuple('ли0·', 'm.'))],
+   [VerbEnding(a_theme_past, AccentedTuple('ле0·', 'm.'))],
+   [VerbEnding(a_theme_past, AccentedTuple('ла0·', 'm.'))],
+   [VerbEnding(a_theme_past, ending_x)],
+   [VerbEnding(AccentedTuple('а·', 'l.p:x.y0'), AccentedTuple('0·~', 'o.k:m.n.t.'))],
+   [VerbEnding(AccentedTuple('а·', 'l.p:x.y0'), AccentedTuple('0·~', 'o.k:m.n.t.'))],
+   [VerbEnding(a_theme_past, ending_smo)],
+   [VerbEnding(a_theme_past, ending_ste)],
+   [VerbEnding(a_theme_past, ending_she)],
+   [VerbEnding(a_theme_past, ending_ti)],
+   [VerbEnding(a_theme_ipf, ending_x)],
+   [VerbEnding(a_theme_ipf, ending_she)],
+   [VerbEnding(a_theme_ipf, ending_she)],
+   [VerbEnding(a_theme_ipf, ending_smo)],
+   [VerbEnding(a_theme_ipf, ending_smo)],
+   [VerbEnding(a_theme_ipf, ending_xu)]
 )
 
 ie_past = Past(
-   VerbEnding(ie_theme_past, AccentedTuple('ʌ', '')),
-   VerbEnding(ie_theme_past, AccentedTuple('ла', '')),
-   VerbEnding(ie_theme_past, AccentedTuple('ло', '')),
-   VerbEnding(ie_theme_past, AccentedTuple('ли', '')),
-   VerbEnding(ie_theme_past, AccentedTuple('ле', '')),
-   VerbEnding(ie_theme_past, AccentedTuple('ла', '')),
-   VerbEnding(ie_theme_past, ending_x),
-   VerbEnding(ie_theme_past, ending_null),
-   VerbEnding(ie_theme_past, ending_null),
-   VerbEnding(ie_theme_past, ending_smo),
-   VerbEnding(ie_theme_past, ending_ste),
-   VerbEnding(ie_theme_past, ending_she),
-   VerbEnding(ie_theme_past, ending_ti),
-   VerbEnding(ie_theme_ipf, ending_x),
-   VerbEnding(ie_theme_ipf, ending_she),
-   VerbEnding(ie_theme_ipf, ending_she),
-   VerbEnding(ie_theme_ipf, ending_smo),
-   VerbEnding(ie_theme_ipf, ending_ste),
-   VerbEnding(ie_theme_ipf, ending_xu)
+   [VerbEnding(ie_theme_past, AccentedTuple('ʌ', ''))],
+   [VerbEnding(ie_theme_past, AccentedTuple('ла', ''))],
+   [VerbEnding(ie_theme_past, AccentedTuple('ло', ''))],
+   [VerbEnding(ie_theme_past, AccentedTuple('ли', ''))],
+   [VerbEnding(ie_theme_past, AccentedTuple('ле', ''))],
+   [VerbEnding(ie_theme_past, AccentedTuple('ла', ''))],
+   [VerbEnding(ie_theme_past, ending_x)],
+   [VerbEnding(ie_theme_past, ending_null)],
+   [VerbEnding(ie_theme_past, ending_null)],
+   [VerbEnding(ie_theme_past, ending_smo)],
+   [VerbEnding(ie_theme_past, ending_ste)],
+   [VerbEnding(ie_theme_past, ending_she)],
+   [VerbEnding(ie_theme_past, ending_ti)],
+   [VerbEnding(ie_theme_ipf, ending_x)],
+   [VerbEnding(ie_theme_ipf, ending_she)],
+   [VerbEnding(ie_theme_ipf, ending_she)],
+   [VerbEnding(ie_theme_ipf, ending_smo)],
+   [VerbEnding(ie_theme_ipf, ending_ste)],
+   [VerbEnding(ie_theme_ipf, ending_xu)]
 )
 
 ova_past = Past(
-   VerbEnding(AccentedTuple('ова~', ''), AccentedTuple('ʌ0·', 'm.')),
-   VerbEnding(AccentedTuple('ова~', ''), AccentedTuple('ла0·', 'm.')),
-   VerbEnding(AccentedTuple('ова~', ''), AccentedTuple('ло0·', 'm.')),
-   VerbEnding(AccentedTuple('ова~', ''), AccentedTuple('ли0·', 'm.')),
-   VerbEnding(AccentedTuple('ова~', ''), AccentedTuple('ле0·', 'm.')),
-   VerbEnding(AccentedTuple('ова~', ''), AccentedTuple('ла0·', 'm.')),
-   VerbEnding(ova_theme_past, ending_x),
-   VerbEnding(AccentedTuple('ова', ''), AccentedTuple('0·~', 'o.m.')),
-   VerbEnding(AccentedTuple('ова', ''), AccentedTuple('0·~', 'o.m.')),
-   VerbEnding(ova_theme_past, ending_smo),
-   VerbEnding(ova_theme_past, ending_ste),
-   VerbEnding(ova_theme_past, ending_she),
-   VerbEnding(ova_theme_past, ending_ti),
-   VerbEnding(ova_theme_ipf, ending_x),
-   VerbEnding(ova_theme_ipf, ending_she),
-   VerbEnding(ova_theme_ipf, ending_she),
-   VerbEnding(ova_theme_ipf, ending_smo),
-   VerbEnding(ova_theme_ipf, ending_ste),
-   VerbEnding(ova_theme_ipf, ending_xu)
+   [VerbEnding(AccentedTuple('ова~', ''), AccentedTuple('ʌ0·', 'm.'))],
+   [VerbEnding(AccentedTuple('ова~', ''), AccentedTuple('ла0·', 'm.'))],
+   [VerbEnding(AccentedTuple('ова~', ''), AccentedTuple('ло0·', 'm.'))],
+   [VerbEnding(AccentedTuple('ова~', ''), AccentedTuple('ли0·', 'm.'))],
+   [VerbEnding(AccentedTuple('ова~', ''), AccentedTuple('ле0·', 'm.'))],
+   [VerbEnding(AccentedTuple('ова~', ''), AccentedTuple('ла0·', 'm.'))],
+   [VerbEnding(ova_theme_past, ending_x)],
+   [VerbEnding(AccentedTuple('ова', ''), AccentedTuple('0·~', 'o.m.'))],
+   [VerbEnding(AccentedTuple('ова', ''), AccentedTuple('0·~', 'o.m.'))],
+   [VerbEnding(ova_theme_past, ending_smo)],
+   [VerbEnding(ova_theme_past, ending_ste)],
+   [VerbEnding(ova_theme_past, ending_she)],
+   [VerbEnding(ova_theme_past, ending_ti)],
+   [VerbEnding(ova_theme_ipf, ending_x)],
+   [VerbEnding(ova_theme_ipf, ending_she)],
+   [VerbEnding(ova_theme_ipf, ending_she)],
+   [VerbEnding(ova_theme_ipf, ending_smo)],
+   [VerbEnding(ova_theme_ipf, ending_ste)],
+   [VerbEnding(ova_theme_ipf, ending_xu)]
 )
 
 iva_past = Past(
-   VerbEnding(iva_theme_past, AccentedTuple('ʌ', '')),
-   VerbEnding(iva_theme_past, AccentedTuple('ла', '')),
-   VerbEnding(iva_theme_past, AccentedTuple('ло', '')),
-   VerbEnding(iva_theme_past, AccentedTuple('ли', '')),
-   VerbEnding(iva_theme_past, AccentedTuple('ле', '')),
-   VerbEnding(iva_theme_past, AccentedTuple('ла', '')),
-   VerbEnding(iva_theme_past, ending_x),
-   VerbEnding(iva_theme_past, AccentedTuple('·', 'k’')),
-   VerbEnding(iva_theme_past, AccentedTuple('·', 'k’')),
-   VerbEnding(iva_theme_past, ending_smo),
-   VerbEnding(iva_theme_past, ending_ste),
-   VerbEnding(iva_theme_past, ending_she),
-   VerbEnding(iva_theme_past, ending_ti),
-   VerbEnding(iva_theme_ipf, ending_x),
-   VerbEnding(iva_theme_ipf, ending_she),
-   VerbEnding(iva_theme_ipf, ending_she),
-   VerbEnding(iva_theme_ipf, ending_smo),
-   VerbEnding(iva_theme_ipf, ending_ste),
-   VerbEnding(iva_theme_ipf, ending_xu)
+   [VerbEnding(iva_theme_past, AccentedTuple('ʌ', ''))],
+   [VerbEnding(iva_theme_past, AccentedTuple('ла', ''))],
+   [VerbEnding(iva_theme_past, AccentedTuple('ло', ''))],
+   [VerbEnding(iva_theme_past, AccentedTuple('ли', ''))],
+   [VerbEnding(iva_theme_past, AccentedTuple('ле', ''))],
+   [VerbEnding(iva_theme_past, AccentedTuple('ла', ''))],
+   [VerbEnding(iva_theme_past, ending_x)],
+   [VerbEnding(iva_theme_past, AccentedTuple('·', 'k’'))],
+   [VerbEnding(iva_theme_past, AccentedTuple('·', 'k’'))],
+   [VerbEnding(iva_theme_past, ending_smo)],
+   [VerbEnding(iva_theme_past, ending_ste)],
+   [VerbEnding(iva_theme_past, ending_she)],
+   [VerbEnding(iva_theme_past, ending_ti)],
+   [VerbEnding(iva_theme_ipf, ending_x)],
+   [VerbEnding(iva_theme_ipf, ending_she)],
+   [VerbEnding(iva_theme_ipf, ending_she)],
+   [VerbEnding(iva_theme_ipf, ending_smo)],
+   [VerbEnding(iva_theme_ipf, ending_ste)],
+   [VerbEnding(iva_theme_ipf, ending_xu)]
 )
 
 #("e", Present),   — todo later
@@ -425,51 +426,51 @@ ending_m = AccentedTuple('м', '')
 ending_sh = AccentedTuple('ш', '')
 
 i_present = Present(
-   VerbEnding(i_theme_prs, ending_m),
-   VerbEnding(i_theme_prs, ending_sh),
-   VerbEnding(i_theme_prs, ending_null),
-   VerbEnding(AccentedTuple('и·\u0304', 'r.r:'), AccentedTuple('мо', 's0')),
-   VerbEnding(AccentedTuple('и·\u0304', 'r.r:'), AccentedTuple('те', 's0')),
-   VerbEnding(AccentedTuple('е·\u0304', 'r.r:s0'), ending_null),
-   VerbEnding(i_theme_imv, ending_null),
-   VerbEnding(i_theme_imv, ending_mo),
-   VerbEnding(i_theme_imv, ending_te)
+   [VerbEnding(i_theme_prs, ending_m)],
+   [VerbEnding(i_theme_prs, ending_sh)],
+   [VerbEnding(i_theme_prs, ending_null)],
+   [VerbEnding(AccentedTuple('и·\u0304', 'r.r:'), AccentedTuple('мо', 's0'))],
+   [VerbEnding(AccentedTuple('и·\u0304', 'r.r:'), AccentedTuple('те', 's0'))],
+   [VerbEnding(AccentedTuple('е·\u0304', 'r.r:s0'), ending_null)],
+   [VerbEnding(i_theme_imv, ending_null)],
+   [VerbEnding(i_theme_imv, ending_mo)],
+   [VerbEnding(i_theme_imv, ending_te)]
 )
 
 je_present = Present(
-   VerbEnding(je_theme_prs, ending_m),
-   VerbEnding(je_theme_prs, ending_sh),
-   VerbEnding(je_theme_prs, ending_null),
-   VerbEnding(AccentedTuple('\u0237е·\u0304', ''), AccentedTuple('мо', 'z0')),
-   VerbEnding(AccentedTuple('\u0237е·\u0304', ''), AccentedTuple('те', 'z0')),
-   VerbEnding(AccentedTuple('\u0237у·\u0304', 'z0'), ending_null),
-   VerbEnding(je_theme_imv, ending_null),
-   VerbEnding(je_theme_imv, ending_mo),
-   VerbEnding(je_theme_imv, ending_te)
+   [VerbEnding(je_theme_prs, ending_m)],
+   [VerbEnding(je_theme_prs, ending_sh)],
+   [VerbEnding(je_theme_prs, ending_null)],
+   [VerbEnding(AccentedTuple('\u0237е·\u0304', ''), AccentedTuple('мо', 'z0'))],
+   [VerbEnding(AccentedTuple('\u0237е·\u0304', ''), AccentedTuple('те', 'z0'))],
+   [VerbEnding(AccentedTuple('\u0237у·\u0304', 'z0'), ending_null)],
+   [VerbEnding(je_theme_imv, ending_null)],
+   [VerbEnding(je_theme_imv, ending_mo)],
+   [VerbEnding(je_theme_imv, ending_te)]
 )
 
 a_present = Present(
-   VerbEnding(a_theme_prs, ending_m),
-   VerbEnding(a_theme_prs, ending_sh),
-   VerbEnding(a_theme_prs, ending_null),
-   VerbEnding(AccentedTuple('а·\u0304', 'n.x.'), AccentedTuple('мо', 'y0')),
-   VerbEnding(AccentedTuple('а·\u0304', 'n.x.'), AccentedTuple('те', 'y0')),
-   VerbEnding(AccentedTuple('а·ју\u0304', 'k:l.n.x.y0'), ending_null),
-   VerbEnding(a_theme_imv, ending_null),
-   VerbEnding(a_theme_imv, ending_mo),
-   VerbEnding(a_theme_imv, ending_te)
+   [VerbEnding(a_theme_prs, ending_m)],
+   [VerbEnding(a_theme_prs, ending_sh)],
+   [VerbEnding(a_theme_prs, ending_null)],
+   [VerbEnding(AccentedTuple('а·\u0304', 'n.x.'), AccentedTuple('мо', 'y0'))],
+   [VerbEnding(AccentedTuple('а·\u0304', 'n.x.'), AccentedTuple('те', 'y0'))],
+   [VerbEnding(AccentedTuple('а·ју\u0304', 'k:l.n.x.y0'), ending_null)],
+   [VerbEnding(a_theme_imv, ending_null)],
+   [VerbEnding(a_theme_imv, ending_mo)],
+   [VerbEnding(a_theme_imv, ending_te)]
 )
 
 uje_present = Present(
-   VerbEnding(uje_theme_prs, ending_m),
-   VerbEnding(uje_theme_prs, ending_sh),
-   VerbEnding(uje_theme_prs, ending_null),
-   VerbEnding(uje_theme_prs, ending_mo),
-   VerbEnding(uje_theme_prs, ending_te),
-   VerbEnding(AccentedTuple('у·ју\u0304', 'k’m.'), ending_null),
-   VerbEnding(uje_theme_imv, ending_null),
-   VerbEnding(uje_theme_imv, ending_mo),
-   VerbEnding(uje_theme_imv, ending_te)
+   [VerbEnding(uje_theme_prs, ending_m)],
+   [VerbEnding(uje_theme_prs, ending_sh)],
+   [VerbEnding(uje_theme_prs, ending_null)],
+   [VerbEnding(uje_theme_prs, ending_mo)],
+   [VerbEnding(uje_theme_prs, ending_te)],
+   [VerbEnding(AccentedTuple('у·ју\u0304', 'k’m.'), ending_null)],
+   [VerbEnding(uje_theme_imv, ending_null)],
+   [VerbEnding(uje_theme_imv, ending_mo)],
+   [VerbEnding(uje_theme_imv, ending_te)]
 )
 
 MP_to_verb_stems: Dict[str, Stems] = dict(
--- verb.py
+++ verb.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict
+from typing import Any, Dict, List
 from .table import Table
 from .paradigms import GramInfo, MP_to_verb_stems
 from .utils import insert, garde, expose, last_vowel_index
@@ -10,7 +10,7 @@ class Verb:
    def __init__(self, key: str, value: Dict[str, Any]) -> None:
       self.key = key
       self.value = value
-      i, t = self.value['i'], self.value['t']
+      i, t = self.value['i'].split(';'), self.value['t']
       self.info = GramInfo(i, t)
       self.is_reflexive = 'Refl' in self.info.other
       self.trunk = self._trunk()
@@ -21,26 +21,29 @@ class Verb:
          form += ' се'
       return form
 
-   def _trunk(self) -> str:
-      accented_verb = garde(self.info.accents[0].accentize(self.key)) # TODO: add loop
-      N = len(infinitive_dict[self.info.MP])
-      if self.info.AP in ['o.', 'a.', 'a:']:
-         return accented_verb[:-N]
-      else:
-         if self.info.MP in ['kappa', 'lambda']:
-            trunk = accented_verb[:-N]
+   def _trunk(self) -> List[str]:
+      result: List[str] = []
+      for number, AP in enumerate(self.info.AP):
+         accented_verb = garde(self.info.accents[number].accentize(self.key)) # TODO: add loop
+         N = len(infinitive_dict[self.info.MP])
+         if AP in ['o.', 'a.', 'a:']:
+            result.append(accented_verb[:-N])
          else:
-            trunk = accented_verb[:-N-1]
-         lvi = last_vowel_index(trunk)
-         if lvi is None:
-            return trunk
-         else:
-            return insert(trunk, {lvi + 1: '·'})
-
-   def _append_morpheme(self, verb_form, ending_part):
+            if self.info.MP in ['kappa', 'lambda']:
+               trunk = accented_verb[:-N]
+            else:
+               trunk = accented_verb[:-N-1]
+            lvi = last_vowel_index(trunk)
+            if lvi is None:
+               result.append(trunk)
+            else:
+               result.append(insert(trunk, {lvi + 1: '·'}))
+      return result
+               
+   def _append_morpheme(self, number, verb_form, ending_part):
       # TODO: sveto: please understand and document
-      if self.info.AP in ending_part.accent:
-         if self.info.AP in ['o.', 'a.', 'a:']:
+      if self.info.AP[number] in ending_part.accent:
+         if self.info.AP[number] in ['o.', 'a.', 'a:']:
             verb_form = verb_form.replace('\u030d', '') # straight
          morpheme = ending_part.morpheme.replace('·', '\u030d') # to straight
       else:
@@ -49,11 +52,16 @@ class Verb:
 
    def conjugate(self) -> Table:
       if self.info.MP in infinitive_dict: # TODO: what if not?
-         for label, ending in MP_to_verb_stems[self.info.MP].labeled_endings:
-            verb_form = self.trunk
-            verb_form = self._append_morpheme(verb_form, ending.theme)
-            verb_form = self._append_morpheme(verb_form, ending.ending)
-            if self.info.AP not in ['o.', 'a.', 'a:']:
-               if '\u030d' not in verb_form: # straight
-                  verb_form = verb_form.replace('·', '\u030d', 1) # to straight
-            yield label, iter([self._expose(verb_form)]) # TODO: multiforms
+         for number, AP in enumerate(self.info.AP):
+            for label, endings in MP_to_verb_stems[self.info.MP].labeled_endings:
+               print(label, type(label))
+               verb_forms: List[str] = []
+               for ending in endings:
+                  verb_form = self.trunk[number]
+                  verb_form = self._append_morpheme(number, verb_form, ending.theme)
+                  verb_form = self._append_morpheme(number, verb_form, ending.ending)
+                  if self.info.AP not in ['o.', 'a.', 'a:']:
+                     if '\u030d' not in verb_form: # straight
+                        verb_form = verb_form.replace('·', '\u030d', 1) # to straight
+                  verb_forms.append(self._expose(verb_form))
+               yield label, verb_forms
