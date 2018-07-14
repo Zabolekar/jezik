7c5747482d27fc9e0877a82c6037c59b721659c6
Zabolekar <syekirin@gmail.com>
2018-07-14 17:27:38 +0300

    querying tables for subtables (like all 1st person forms or all sg aor forms)

--- table.py
+++ table.py
@@ -1,21 +1,25 @@
-from typing import Iterator, List, Tuple
+from typing import Iterable, Iterator, List, Tuple
 
 Form = str
 Multiform = List[Form]
 LabeledMultiform = Tuple[str, Multiform]
 
 class Table:
-   def __init__(self, caption: str, data: Iterator[LabeledMultiform]) -> None:
+   def __init__(self, caption: str, data: Iterable[LabeledMultiform]) -> None:
       self.caption = caption
       self._data = list(data)
 
-   def __getitem__(self, name):
+   def __getitem__(self, query: str) -> "Table": # TODO: you know what to do when we switch to 3.7
+      result = []
+      v = query.split()
       for form_name, forms in self:
-         if name == form_name:
-            return forms
+         w = form_name.split()
+         if all(s in w for s in v):
+            result.append((form_name, forms))
+      return Table(f"{self.caption} [{query}]", result)
 
-   def __repr__(self):
+   def __repr__(self) -> str:
       return str(self._data)
 
-   def __iter__(self):
+   def __iter__(self) -> Iterator[LabeledMultiform]:
       return iter(self._data)
