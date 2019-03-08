7e246b1bb8a4999970f15c8b072ebc80fcdab1ee
Zabolekar <syekirin@gmail.com>
2019-03-08 10:14:29 +0100

    adding comments

--- data/multidict.py
+++ data/multidict.py
@@ -56,7 +56,7 @@ def inner_to_outer(s: str, accent: str) -> Iterator[Tuple[str, str]]:
    tmp_list = [tmp.replace('Ъ', ''), tmp.replace('Ъ', 'ъ')] if 'Ъ' in tmp else [tmp]
    for input_yat in ["ekav", "jekav", "ijekav"]:
       for item in tmp_list:
-         accented_token = garde(accentize(item, accent_dict.r, accent_dict.v))
+         accented_token = garde(accentize(item, accent_dict.r, accent_dict.v)) # TODO: this does not depend on yat, can be moved out of the loop (and inner loop should be made outer loop before that)
          exposed_token = expose(accented_token, yat=input_yat)
          deaccentized_token = deaccentize(exposed_token)
          yield deaccentized_token, input_yat
