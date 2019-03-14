8f1c0e4ec5ff04bc38c2b3b06a9b5de6a561c792
sveto <niekas33@gmail.com>
2019-03-14 22:19:10 +0200

    new file for better code readability

---
+++ lookup/charutils.py
@@ -0,0 +1,14 @@
+cring = "\u0325"
+cmacron = "\u0304"
+cstraight = "\u030d"
+cacute = "\u0301"
+cgrave = "\u0300"
+cdoublegrave = "\u030f"
+ccircumflex = "\u0311"
+cbreve = "\u0306"
+all_vowels = f"АаЕеИиОоУуӤӥŒœꙒꙓѢѣAaEeIiOoUu{cring}"
+any_vowel = f"[{all_vowels}]"
+any_of_four_accents = f"[{cgrave}{cacute}{ccircumflex}{cdoublegrave}]"
+all_accent_marks =  [cring, cmacron, cstraight,
+                    cacute, cgrave, cdoublegrave,
+                    ccircumflex, cbreve, '!']
