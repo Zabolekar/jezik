9f837d4c3ddd6d0c9a8c19815bd9a614cf24e6b5
sveto <niekas33@gmail.com>
2019-06-08 12:59:22 +0300

    a bit of redesigning, add 'about' page

--- __init__.py
+++ __init__.py
@@ -29,6 +29,10 @@ def page_not_found(_):
 def random():
    return render_template("results.html", tables=random_lookup())
 
+@app.route("/about")
+def about():
+   return render_template("about.html")
+
 @app.route("/favicon.ico")
 def favicon():
     return send_from_directory(os.path.join(app.root_path, "static"),
